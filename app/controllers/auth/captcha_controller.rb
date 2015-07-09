3fe8c16e1faceb846f84dc7f1b9a1f41ff549fe6
rex.houy@gmail.com <rex.houy@gmail.com>
2015-07-09 20:31:31 +0800

    CAPTCHA length

--- app/controllers/auth/captcha_controller.rb
+++ app/controllers/auth/captcha_controller.rb
@@ -5,7 +5,7 @@ class Auth::CaptchaController < ApplicationController
                 captcha = Captcha.find_by_tel(params[:tel])
                 captcha ||= Captcha.new
                 captcha.tel = params[:tel]
-                captcha.register_token = SecureRandom.random_number(10**6)
+                captcha.register_token = SecureRandom.random_number(10**6).to_s.rjust(6,"0")
                 captcha.register_sent_at = DateTime.now + 10.minutes # Expire in 10 minutes
 
                 SmsService.new.send_captcha(captcha.register_token, captcha.tel)
