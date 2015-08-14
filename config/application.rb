727dd295b280fb93d74483683ffbe11f26cf324d
rex.houy@gmail.com <rex.houy@gmail.com>
2015-08-14 16:53:48 +0800

    set timezone

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Webstore
 
                 # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
                 # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-                # config.time_zone = 'Central Time (US & Canada)'
+	        config.time_zone = 'Chongqing'
 
                 # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
                 # config.i18n.load_path += Dir[Rails.root.join('webstore/config/', 'locales', '*.{rb,yml}').to_s]
