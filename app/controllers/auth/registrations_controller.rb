837b5a90c9ac1569c4523675b3e49aef0793cf5d
rex.houy@gmail.com <rex.houy@gmail.com>
2015-08-28 15:38:57 +0800

    bug fix

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -2,7 +2,7 @@
 class Auth::RegistrationsController < Devise::RegistrationsController
         # before_filter :configure_sign_up_params, only: [:create]
         # before_filter :configure_account_update_params, only: [:update]
-        # before_filter :check_captcha, only: [:create]
+        before_filter :check_captcha, only: [:create]
 
         # GET /resource/sign_up
         def new
