0cce33dc14237c3f6edef42bb4e9fd6eed10d395
rex.houy@gmail.com <rex.houy@gmail.com>
2015-09-15 17:28:44 +0800

    bug fix

--- app/helpers/admin/articles_helper.rb
+++ app/helpers/admin/articles_helper.rb
@@ -1,18 +1,2 @@
 module Admin::ArticlesHelper
-        def category_options
-                root = Category.root
-                options = ""
-                root.each do |c|
-                        if c.children.empty?
-                                options << "<option value='#{c.id}'>#{c.name}</option>"
-                        else
-                                options << "<optgroup label='#{c.name}'>"
-                                c.children.each do |sub|
-                                        options << "<option value='#{sub.id}'>#{sub.name}</option>"
-                                end
-                                options << "</optgroup>"
-                        end
-                end
-                options.html_safe
-        end
 end
--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,2 +1,18 @@
 module Admin::ProductsHelper
+        def category_options(category_id)
+                root = Category.root
+                options = ""
+                root.each do |c|
+                        if c.children.empty?
+                                options << "<option value='#{c.id}' #{'selected' if c.id.eql? category_id}>#{c.name}</option>"
+                        else
+                                options << "<optgroup label='#{c.name}'>"
+                                c.children.each do |sub|
+                                        options << "<option value='#{sub.id}' #{'selected' if sub.id.eql? category_id}>#{sub.name}</option>"
+                                end
+                                options << "</optgroup>"
+                        end
+                end
+                options.html_safe
+        end
 end
