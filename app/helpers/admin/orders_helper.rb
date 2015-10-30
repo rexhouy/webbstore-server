db69c6dab0c46f5c96c6446c6aca486994845c70
rex.houy@gmail.com <rex.houy@gmail.com>
2015-10-30 12:43:38 +0800

    bug fix

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -19,7 +19,7 @@ module Admin::OrdersHelper
         end
 
         def order_status_change_operator(history)
-                "系统" if history.operator.nil?
+                return "系统" if history.operator.nil?
                 history.operator.tel
         end
 
