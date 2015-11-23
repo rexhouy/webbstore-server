9fa863eb9f76a07994ddf92225f4fa034aed35a2
rex.houy@gmail.com <rex.houy@gmail.com>
2015-11-23 20:41:30 +0800

    bug fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -82,12 +82,12 @@ class OrdersController < ApiController
         ## Redirect to a view and let the view handle the payment.
         def payment_redirect_url(order)
                 return "/orders/#{order.id}" if order.paid? || order.offline_pay?
-                return "http://#{Rails.application.config.domain}/payment/alipay/redirect?id=#{order.id}" if order.alipay?
+                return "https://#{Rails.application.config.domain}/payment/alipay/redirect?id=#{order.id}" if order.alipay?
                 if order.wechat?
                         if current_user.wechat_openid.present?
-                                return "http://#{Rails.application.config.domain}/payment/wechat/redirect?open_id=#{current_user.wechat_openid}&state=#{order.id}"
+                                return "https://#{Rails.application.config.domain}/payment/wechat/redirect?open_id=#{current_user.wechat_openid}&state=#{order.id}"
                         end
-                        return WechatService.new.auth_url("http://#{Rails.application.config.domain}/payment/wechat/redirect", order.id)
+                        return WechatService.new.auth_url("https://#{Rails.application.config.domain}/payment/wechat/redirect", order.id)
                 end
                 render_404
         end
