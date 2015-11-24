19811b7c8b3155c3c62508c0e5001202e7d04194
rex.houy@gmail.com <rex.houy@gmail.com>
2015-11-24 13:19:44 +0800

    bug fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class CategoriesController < ApiController
 
         def index
@@ -8,6 +9,10 @@ class CategoriesController < ApiController
         def owner
                 Rails.application.config.owner
         end
-
+        def set_header
+                @title = "商品分类"
+                category = session[:category].present? ? session[:category]["id"] : 1
+                @back_url = "/products?category=#{category || 1}"
+        end
 
 end
