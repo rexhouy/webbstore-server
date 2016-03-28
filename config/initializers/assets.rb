3895a5168466bcf0b0a2cfe4d897937edb5fc50f
rex.houy@gmail.com <rex.houy@gmail.com>
2016-03-28 14:31:16 +0800

    style change

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( admin.css )
+Rails.application.config.assets.precompile += %w( gift.css )
 # Rails.application.config.assets.precompile += %w( admin.js )
 Rails.application.config.assets.precompile += ['*.js']
