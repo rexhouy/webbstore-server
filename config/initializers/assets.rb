4c64163149cb4435f9047759abcf484a022d6680
rex.houy@gmail.com <rex.houy@gmail.com>
2016-04-03 17:46:34 +0800

    bug fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 # Rails.application.config.assets.precompile += %w( admin.css )
 # Rails.application.config.assets.precompile += %w( admin.js )
-Rails.application.config.assets.precompile += ['*.js', '*.css']
+# Rails.application.config.assets.precompile += ['*.js', '*.css']
+Rails.application.config.assets.precompile += %w( admin.css )
+Rails.application.config.assets.precompile += ['*.js']
